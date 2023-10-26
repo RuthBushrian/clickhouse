b365a4edc72407286707749880f3cfc0d632f094
RuthBushrian <36213865298@mby.co.il>
2023-10-26 11:06:47 +0300

    add sql files

---
+++ sql/tabels.sql
@@ -0,0 +1,14 @@
+CREATE TABLE 
+IF NOT EXISTS new_database.new_nice_table 
+(
+    "id UInt32, "
+    "first_name String, "
+    "last_name String, "
+    "email String, "
+    "age Int32, "
+    "salary Float64, "
+    "is_active Boolean, "
+    "registration_date Date, "
+    "last_login DateTime"
+)
+ENGINE = MergeTree() ORDER BY id

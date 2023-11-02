cdc90d7bd72af3064a4a663784e70d373ae0169e
RuthBushrian <36213865298@mby.co.il>
2023-11-02 16:47:52 +0200

    A

--- sql/tabels.sql
+++ sql/tabels.sql
@@ -11,3 +11,18 @@ CREATE TABLE IF NOT EXISTS new_database_from_action.new_nice_table
     last_login DateTime
 ) 
 ENGINE = MergeTree() ORDER BY id;
+
+
+CREATE TABLE IF NOT EXISTS new_database_from_action.new_nice_table2 
+(
+    id UInt32, 
+    first_name String, 
+    last_name String, 
+    email String, 
+    age Int32, 
+    salary Float64, 
+    is_active Boolean, 
+    registration_date Date, 
+    last_login DateTime
+) 
+ENGINE = MergeTree() ORDER BY id;
